085c67481db0e447f306c4f46fd06b4bbb00b51a
ThuongMinhMai <thuongmtmse170103@fpt.edu.vn>
2024-09-13 13:07:38 +0700

    add layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { Route, Routes } from 'react-router-dom'
 import HomePage from './components/global/templates/HomePage'
 import RoomPage from './components/global/templates/RoomPage'
+import RootLayout from './components/global/templates/RootLayout'
+import NotFound from './components/global/organisms/NotFound'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<HomePage />} />
+      <Route element={<RootLayout/>}>
+
       <Route path='/room' element={<RoomPage />} />
+      </Route>
+
+      <Route path='*' element={<NotFound />} />
+
     </Routes>
   )
 }
---
+++ src/components/global/templates/RootLayout.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom'
+import Header from '../../../components/global/organisms/Header'
+import Footer from '../../../components/global/organisms/Footer'
+
+function RootLayout() {
+  return (
+    <div className="h-screen relative">
+      <Header />
+      <div className="flex min-h-[70%] pt-10">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default RootLayout
