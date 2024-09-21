1295623cc16a9b21027dc26b9a5cf0290d62bbd9
ThuongMinhMai <thuongmtmse170103@fpt.edu.vn>
2024-09-22 01:43:08 +0700

    add server extra for form

--- src/components/global/organisms/Detail.tsx
+++ src/components/global/organisms/Detail.tsx
@@ -179,7 +179,7 @@ function Detail() {
             <div className='h-[1px] w-full bg-[#647C6C] my-8'></div>
           </div>
 
-          <div className='flex-[2 h-fit sticky top-[95px]'>
+          <div className='flex-[2.5] h-fit sticky top-[95px]'>
             <BookingForm storeOpenTime='09:00' storeCloseTime='18:00' bookedSlots={bookedSlots} />,
           </div>
         </div>
