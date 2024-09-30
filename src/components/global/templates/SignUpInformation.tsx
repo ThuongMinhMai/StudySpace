5b4cb741a6805572d16a129f9729c1e5dc22ad09
ThuongMinhMai <thuongmtmse170103@fpt.edu.vn>
2024-10-01 00:01:33 +0700

    fetch sign up api user and config form sign up supllier

--- src/components/global/templates/SignUpInformation.tsx
+++ src/components/global/templates/SignUpInformation.tsx
@@ -1,20 +1,58 @@
-import { Button, Col, ConfigProvider, DatePicker, Form, Input, Radio, Row } from 'antd'
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Button, Col, ConfigProvider, DatePicker, Form, Input, Radio, Row, TimePicker } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 import axios from 'axios' // Assuming axios is used for making API requests
 import logoMini from '../../../assets/LOGO SS 04.png'
 import moment from 'moment'
+import studySpaceAPI from '../../../lib/studySpaceAPI'
+import { toast } from 'sonner'
 function SignUpInformation() {
   const [role, setRole] = useState<'supplier' | 'user'>('user')
   const [loading, setLoading] = useState(false)
+  const [token, setToken] = useState<string | null>(null)
+  const [email, setEmail] = useState<string | null>(null)
+  const location = useLocation()
+  const navigate = useNavigate()
+  const [form] = Form.useForm()
   const disableDate = (current: any) => {
     return current && current.isAfter(moment())
   }
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search)
+    const extractedToken = params.get('token')
+    const extractedEmail = params.get('email') // Assuming email is passed via URL
+
+    if (extractedToken) {
+      setToken(extractedToken)
+    }
+
+    if (extractedEmail) {
+      setEmail(extractedEmail)
+    }
+  }, [location])
+
+  // Ensure email is updated once it's set in state
+  useEffect(() => {
+    if (email) {
+      form.setFieldsValue({ email: email }) // Set the email value in the form
+    }
+  }, [email, form])
+  console.log('token nè', token, email)
+  const formatValues = (values: any) => {
+    return {
+      ...values,
+      openTime: values.openTime ? values.openTime.toISOString() : null,
+      closeTime: values.closeTime ? values.closeTime.toISOString() : null
+    }
+  }
+
   const onSupplierFinish = async (values: any) => {
-    console.log('Supplier Form Values: ', values)
+    const formattedValues = formatValues(values)
+    console.log('Supplier Form Values: ', formattedValues)
     setLoading(true)
     try {
-      const response = await axios.post('/api/supplier/signup', values)
+      const response = await axios.post(`/Accounts?token=${token}`, values)
       console.log('Supplier Signup Response: ', response.data)
     } catch (error) {
       console.error('Supplier Signup Failed:', error)
@@ -24,13 +62,36 @@ function SignUpInformation() {
   }
 
   const onUserFinish = async (values: any) => {
-    console.log('User Form Values: ', values)
+    if (!token) {
+      toast.error('Token is missing')
+      return
+    }
+    if (!email) {
+      toast.error('Email is missing')
+      return
+    }
+    // const formattedValues = formatValues(values)
+    const payload = {
+      name: values.fullName,
+      roleName: 'user',
+      email: values.email,
+      password: values.password,
+      phone: values.phone,
+      address: values.address,
+      gender: values.gender,
+      dob: values.dob.toISOString() // Formatting Date of Birth to ISO string
+    }
+    console.log('User Form Values: ', payload)
+
     setLoading(true)
     try {
-      const response = await axios.post('/api/user/signup', values)
+      const response = await studySpaceAPI.post(`/Accounts?token=${token}`, payload)
       console.log('User Signup Response: ', response.data)
+      toast.success('Sign up successfully')
+      navigate('/')
     } catch (error) {
       console.error('User Signup Failed:', error)
+      toast.error('Sign Up Failed!Please try again!')
     } finally {
       setLoading(false)
     }
@@ -79,45 +140,197 @@ function SignUpInformation() {
 
             {/* Supplier Form */}
             {role === 'supplier' && (
-              <Form name='supplier_sign_up' onFinish={onSupplierFinish} layout='vertical' className=''>
-                <Form.Item
-                  label={<span className='font-medium'>Business Name</span>}
-                  name='businessName'
-                  rules={[{ required: true, message: 'Please input your business name!' }]}
-                >
-                  <Input placeholder='Enter your business name' size='middle' />
-                </Form.Item>
+              <Form name='supplier_sign_up' form={form} onFinish={onSupplierFinish} layout='vertical' className=''>
+                <Row gutter={16}>
+                  <Col span={12}>
+                    <Form.Item
+                      label={<span className='font-medium'>Business Name</span>}
+                      name='name'
+                      rules={[{ required: true, message: 'Please input your business name!' }]}
+                    >
+                      <Input placeholder='Enter your business name' size='middle' />
+                    </Form.Item>
+                  </Col>
 
-                <Form.Item
-                  label={<span className='font-medium'>Business Address</span>}
-                  name='businessAddress'
-                  rules={[{ required: true, message: 'Please input your business address!' }]}
-                >
-                  <Input placeholder='Enter your business address' size='middle' />
-                </Form.Item>
+                  <Col span={12}>
+                    <Form.Item
+                      label={<span className='font-medium'>Business Address</span>}
+                      name='address'
+                      rules={[{ required: true, message: 'Please input your business address!' }]}
+                    >
+                      <Input placeholder='Enter your business address' size='middle' />
+                    </Form.Item>
+                  </Col>
+                </Row>
 
-                <Form.Item
-                  label={<span className='font-medium'>Email</span>}
-                  name='email'
-                  rules={[
-                    { required: true, message: 'Please input your email!' },
-                    { type: 'email', message: 'Please enter a valid email!' }
-                  ]}
-                >
-                  <Input type='email' placeholder='Enter your email' size='middle' />
-                </Form.Item>
+                <Row gutter={16}>
+                  <Col span={12}>
+                    <Form.Item
+                      label={<span className='font-medium'>Phone Number</span>}
+                      name='phone'
+                      rules={[
+                        { required: true, message: 'Please input your phone number!' },
+                        {
+                          pattern: /^0\d{9}$/,
+                          message: 'Phone number must start with 0 and contain 10 digits!'
+                        }
+                      ]}
+                    >
+                      <Input placeholder='Enter your phone number' size='middle' />
+                    </Form.Item>
+                  </Col>
 
-                <Form.Item>
-                  <Button type='primary' htmlType='submit' size='middle' block loading={loading}>
-                    Sign Up as Supplier
-                  </Button>
-                </Form.Item>
+                  <Col span={12}>
+                    <Form.Item
+                      label={<span className='font-medium'>Email</span>}
+                      name='email'
+                      rules={[
+                        { required: true, message: 'Please input your email!' },
+                        { type: 'email', message: 'Please enter a valid email!' }
+                      ]}
+                    >
+                      <Input
+                        type='email'
+                        placeholder='Enter your email'
+                        size='middle'
+                        className='cursor-not-allowed'
+                        readOnly
+                      />
+                    </Form.Item>
+                  </Col>
+                </Row>
+
+                <Row gutter={16}>
+                  <Col span={12}>
+                    <Form.Item
+                      label={<span className='font-medium'>Password</span>}
+                      name='password'
+                      rules={[
+                        { required: true, message: 'Please input your password!' },
+                        { min: 6, message: 'Password must be at least 6 characters long!' },
+                        {
+                          pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
+                          message:
+                            'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character!'
+                        }
+                      ]}
+                      hasFeedback
+                    >
+                      <Input.Password placeholder='Enter your password' size='middle' />
+                    </Form.Item>
+                  </Col>
+
+                  <Col span={12}>
+                    <Form.Item
+                      label={<span className='font-medium'>Confirm Password</span>}
+                      name='confirmPassword'
+                      dependencies={['password']}
+                      hasFeedback
+                      rules={[
+                        { required: true, message: 'Please confirm your password!' },
+                        ({ getFieldValue }) => ({
+                          validator(_, value) {
+                            if (!value || getFieldValue('password') === value) {
+                              return Promise.resolve()
+                            }
+                            return Promise.reject(new Error('Passwords do not match!'))
+                          }
+                        })
+                      ]}
+                    >
+                      <Input.Password placeholder='Confirm your password' size='middle' />
+                    </Form.Item>
+                  </Col>
+                </Row>
+                <Row gutter={16}>
+                  <Col span={12}>
+                    <Form.Item
+                      label={<span className='font-medium'>Open Time</span>}
+                      name='openTime'
+                      rules={[{ required: true, message: 'Please select open time!' }]}
+                    >
+                      <TimePicker className='w-full' format='HH:mm' />
+                    </Form.Item>
+                  </Col>
+
+                  <Col span={12}>
+                    <Form.Item
+                      label={<span className='font-medium'>Close Time</span>}
+                      name='closeTime'
+                      rules={[{ required: true, message: 'Please select close time!' }]}
+                    >
+                      <TimePicker className='w-full' format='HH:mm' />
+                    </Form.Item>
+                  </Col>
+                </Row>
+                <Row gutter={16}>
+                  <Col span={12}>
+                    <Form.Item
+                      label={<span className='font-medium'>Is Overnight Service Available?</span>}
+                      name='isOverNight'
+                      rules={[{ required: true, message: 'Please choose an option!' }]}
+                    >
+                      <Radio.Group>
+                        <Radio value={1}>Yes</Radio>
+                        <Radio value={0}>No</Radio>
+                      </Radio.Group>
+                    </Form.Item>
+                  </Col>
+
+                  <Col span={12}>
+                    <Form.Item
+                      label={<span className='font-medium'>Description</span>}
+                      name='description'
+                      rules={[{ required: true, message: 'Please input a description!' }]}
+                    >
+                      <Input.TextArea placeholder='Enter a brief description' size='middle' rows={4} />
+                    </Form.Item>
+                  </Col>
+                </Row>
+
+                <Row gutter={16}>
+                  <Col span={12}>
+                    <Form.Item
+                      label={<span className='font-medium'>Tax Number</span>}
+                      name='taxNumber'
+                      rules={[
+                        { required: true, message: 'Please input your tax number!' },
+                        { pattern: /^[0-9]+$/, message: 'Tax number must be numeric!' }
+                      ]}
+                    >
+                      <Input placeholder='Enter your tax number' size='middle' />
+                    </Form.Item>
+                  </Col>
+
+                  <Col span={12}>
+                    <Form.Item
+                      label={<span className='font-medium'>Postal Number</span>}
+                      name='postalNumber'
+                      rules={[
+                        { required: true, message: 'Please input your postal number!' },
+                        { pattern: /^[0-9]+$/, message: 'Postal number must be numeric!' }
+                      ]}
+                    >
+                      <Input placeholder='Enter your postal number' size='middle' />
+                    </Form.Item>
+                  </Col>
+                </Row>
+
+                <Row gutter={16} justify='center' align='middle'>
+                  <Col span={12}>
+                    <Form.Item>
+                      <Button type='primary' htmlType='submit' size='middle' block loading={loading}>
+                        Sign Up as Supplier
+                      </Button>
+                    </Form.Item>
+                  </Col>
+                </Row>
               </Form>
             )}
 
             {/* User Form */}
             {role === 'user' && (
-              <Form name='user_sign_up' onFinish={onUserFinish} layout='vertical'>
+              <Form name='user_sign_up' form={form} onFinish={onUserFinish} layout='vertical'>
                 <Row gutter={16}>
                   <Col span={12}>
                     <Form.Item
@@ -165,7 +378,13 @@ function SignUpInformation() {
                         { type: 'email', message: 'Please enter a valid email!' }
                       ]}
                     >
-                      <Input type='email' placeholder='Enter your email' size='middle' />
+                      <Input
+                        type='email'
+                        placeholder='Enter your email'
+                        size='middle'
+                        readOnly
+                        className='cursor-not-allowed' // Set cursor to 'not-allowed'
+                      />
                     </Form.Item>
                   </Col>
                 </Row>
@@ -231,9 +450,9 @@ function SignUpInformation() {
                       rules={[{ required: true, message: 'Please select your gender!' }]}
                     >
                       <Radio.Group>
-                        <Radio value='male'>Male</Radio>
-                        <Radio value='female'>Female</Radio>
-                        <Radio value='other'>Other</Radio>
+                        <Radio value='nam'>Male</Radio>
+                        <Radio value='nu'>Female</Radio>
+                        <Radio value='none'>Other</Radio>
                       </Radio.Group>
                     </Form.Item>
                   </Col>
